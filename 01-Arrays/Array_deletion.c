4335c8050199028a9290262f6efd7aaa8c964f17
Pratik Dhumale <dhumalepratik6@gmail.com>
2026-04-07 18:14:51 +0530

    Added deletion in array program

---
+++ 01-Arrays/Array_deletion.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main()
+{
+      int array[],i,j,n,val,found = 0;
+  
+      printf("Enter the number of element : ");       // Input the size of array
+      scanf("%d",&n);
+
+      printf("Enter the array element : ");       // Input the array element 
+      for(i = 0;i < n;i++)
+        {
+            scanf("%d",&array[i]]);
+        }
+
+      printf("Enter the element to delete : ");         // Input the delete value
+      scanf("%d",&val);
+
+      for(i = 0;i < n;i++)
+        {
+                if(array[i] == val)
+                {
+                      	found = 1;
+												for(j = i;j < n-1;j++)
+													{
+																	array[j] = array[j+1];
+													}
+													n--;
+													break;
+                }
+        }
+	
+			if(found == 0)                // Display output
+			{
+							printf("Element not found\n");
+			}
+			else
+			{
+							printf("Array after deletion : ");
+							for(i = 0;i < n;i++)
+								{
+ 												printf("%d ",array[i]);
+								}
+			}
+	
+			return 0;
+}
