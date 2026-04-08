9c66e2ab7fb2bb2c18aa7b860d8945ffa2e163c9
Pratik Dhumale <dhumalepratik6@gmail.com>
2026-04-08 17:58:22 +0530

    Added Even & Odd Element in array program

---
+++ 01-Arrays/Count_Even_Odd.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int array[20],i,n;
+    int odd = 0, even = 0;   // store the number of even & odd element
+
+    printf("Enter the number of element : ");
+    scanf("%d",&n);
+
+    printf("Enter the element : ");
+    for(i = 0;i < n;i++)
+    {
+      scanf("%d",&array[i]);
+    }
+
+    for(i = 0;i < n;i++)              // Count even and odd
+    {
+        if(array[i] % 2 == 0)
+        {
+            even++;
+        }
+        else 
+        {
+            odd++;
+        }
+    }
+    
+    printf("Total Even elements in  the array : %d\n",even);
+    printf("Total Odd elements in the array : %d\n",odd);
+
+    getch();
+}
